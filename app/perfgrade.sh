ae25145f44800c06ce040aed6a6311a8fa7a480d
Jack O'Sullivan <jackos1998@gmail.com>
2021-04-02 16:36:31 +0100

    app: Update Dockerfile and script

--- app/perfgrade.sh
+++ app/perfgrade.sh
@@ -6,5 +6,10 @@ die() {
     exit 1
 }
 
-[ $# -ne 2 ] && die "usage: $0 <pipeline dir> <pipeline>"
-exec docker run --rm -ti --privileged -v /dev/bus/usb:/dev/bus/usb -v "$1:/pipelines" ghcr.io/devplayer0/perfgrade "/pipelines/$2"
+[ $# -ne 1 ] && die "usage: $0 <pipeline>"
+
+abs="$(realpath $1)"
+vol="$(dirname $abs)"
+pipeline="$(basename $abs)"
+
+exec docker run --rm -ti --privileged -v /dev/bus/usb:/dev/bus/usb -v "$vol:/work" ghcr.io/devplayer0/perfgrade "/work/$pipeline"
