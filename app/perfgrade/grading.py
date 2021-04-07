f732d4641565d1d3f34f758280a09fdb5e3af2da
Jack O'Sullivan <jackos1998@gmail.com>
2021-04-07 02:37:31 +0100

    app: Add bucket grade graph

--- app/perfgrade/grading.py
+++ app/perfgrade/grading.py
@@ -2,6 +2,8 @@ from collections.abc import Mapping
 import difflib
 
 from box import Box
+import numpy as np
+import matplotlib.pyplot as plt
 
 from .step import Step
 
@@ -10,26 +12,51 @@ class BucketGrade(Step):
 
     def run(self, ctx: Mapping):
         input_ = Box(self.input)
+
+        fig, ax = plt.subplots()
+        ax.set_title(input_.get('title', 'Grade curve'))
+        ax.set_ylabel('Grade')
+        if input_.get('xlabel'):
+            ax.set_xlabel(input_.xlabel)
+
         for i, bucket in enumerate(input_.buckets):
-            if input_.value < bucket.max:
-                if i == 0:
-                    prev_max = 0
-                    max_grade = 1
-                else:
-                    max_grade = bucket.max_grade
-                    prev_max = input_.buckets[i-1].max
+            if i == 0:
+                prev_max = 0
+                max_grade = 1
+            else:
+                max_grade = bucket.max_grade
+                prev_max = input_.buckets[i-1].max
 
-                if i == len(input_.buckets) - 1:
-                    next_max_grade = 0
-                else:
-                    next_max_grade = input_.buckets[i+1].max_grade
+            if i == len(input_.buckets) - 1:
+                next_max_grade = 0
+            else:
+                next_max_grade = input_.buckets[i+1].max_grade
 
-                x = 1 - ((input_.value - prev_max) / (bucket.max - prev_max))
-                f = bucket.get('f', lambda x: x)
-                self.output = f(x) * (max_grade - next_max_grade) + next_max_grade
-                return
+            # f should map 0-1 -> 0-1
+            f = bucket.get('f', lambda x: x)
+            # f_bucket maps 0-1 -> grade in the bucket
+            f_bucket = lambda x: f(x) * (max_grade - next_max_grade) + next_max_grade
 
-        self.output = 0
+            global_space = np.linspace(prev_max, bucket.max, num=64)
+            bucket_space = np.linspace(1, 0, num=64)
+            ax.plot(global_space, f_bucket(bucket_space), color='m')
+
+            ax.plot([bucket.max]*2, [0, 1], label=bucket.get('label'))
+
+            x = input_.value
+            if self.output is None and x < bucket.max:
+                # x_bucket is input absolute value mapped to 0-1 between the bucket max/min
+                x_bucket = 1 - ((x - prev_max) / (bucket.max - prev_max))
+                grade = f_bucket(x_bucket)
+
+                ax.scatter(x, grade, color='r')
+                ax.annotate(f'{grade:.2}', (x, grade), xytext=(4, 4), textcoords='offset points')
+
+                self.output = grade
+
+        ax.legend()
+        if input_.get('graph_file'):
+            fig.savefig(input_.graph_file)
 
 class DiffException(Exception):
     pass
